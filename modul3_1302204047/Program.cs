2a26aa989d75c0b03d373412359c78a789eaefb1
anugrahSA <anugrahnugi321@gmail.com>
2022-03-12 14:19:37 +0800

    menambahkan class KodeBuah

--- modul3_1302204047/Program.cs
+++ modul3_1302204047/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace modul3_1302204047
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.Buah.Apel));
+            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.Buah.Aprikot));
+            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.Buah.Alpukat));
+            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.Buah.Pisang));
+            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.Buah.Paprika));
+            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.Buah.Blackberry));
+            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.Buah.Ceri));
+            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.Buah.Kelapa));
+            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.Buah.Jagung));
+            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.Buah.Kurma));
+            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.Buah.Durian));
+            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.Buah.Anggur));
+            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.Buah.Melon));
+            Console.WriteLine(KodeBuah.getKodeBuah(KodeBuah.Buah.Semangka));
+        }
+    }
+}
